fc1ecc0bef801668ea08fef766c968b7851ca046
Spiritfaer <30905488+Spiritfaer@users.noreply.github.com>
2020-11-30 22:57:10 +0200

    Add fragment logic in MainActivity and edit activity_main.xml

--- app/src/main/java/com/example/tour_guide/MainActivity.java
+++ app/src/main/java/com/example/tour_guide/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.tour_guide;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -11,34 +14,13 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final ArrayList<Article> articles;
-
-    {
-        articles = new ArrayList<>();
-        articles.add(new Article());
-        articles.add(new Article());
-        articles.add(new Article());
-        articles.add(new Article());
-        articles.add(new Article());
-        articles.add(new Article());
-        articles.add(new Article());
-        articles.add(new Article());
-        articles.add(new Article());
-        articles.add(new Article());
-        articles.add(new Article());
-    }
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.item_list);
+        setContentView(R.layout.activity_main);
 
-        //find a listView to fills with our articles
-        ListView listView = findViewById(R.id.item_list_id);
-        //Create our adapter
-        ArticleAdapter adapter = new ArticleAdapter(this, R.layout.item, articles);
-        //Link listView and adapter
-        listView.setAdapter(adapter);
+        ViewPager view = findViewById(R.id.view_pager);
+        CategoriesFragmentPagerAdapter adapter = new CategoriesFragmentPagerAdapter(getSupportFragmentManager());
+        view.setAdapter(adapter);
     }
 }
