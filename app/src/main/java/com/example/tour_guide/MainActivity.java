4769c4f5b2eb59f364d22c8a8d07774d5c86fba7
Spiritfaer <30905488+Spiritfaer@users.noreply.github.com>
2020-12-01 10:09:33 +0200

    Add simple JSON logic

--- app/src/main/java/com/example/tour_guide/MainActivity.java
+++ app/src/main/java/com/example/tour_guide/MainActivity.java
@@ -6,13 +6,32 @@ import androidx.viewpager.widget.ViewPager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final ArrayList<Article> articles;
+
+    {
+        articles = new ArrayList<>();
+        articles.add(new Article());
+        articles.add(new Article());
+        articles.add(new Article());
+        articles.add(new Article());
+        articles.add(new Article());
+        articles.add(new Article());
+        articles.add(new Article());
+        articles.add(new Article());
+        articles.add(new Article());
+        articles.add(new Article());
+        articles.add(new Article());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +41,16 @@ public class MainActivity extends AppCompatActivity {
         ViewPager view = findViewById(R.id.view_pager);
         CategoriesFragmentPagerAdapter adapter = new CategoriesFragmentPagerAdapter(getSupportFragmentManager());
         view.setAdapter(adapter);
+    }
+
+    public void save(View view){
+
+        boolean result = JSONHelper.exportToJSON(this, articles);
+        if(result){
+            Toast.makeText(this, "Данные сохранены", Toast.LENGTH_LONG).show();
+        }
+        else{
+            Toast.makeText(this, "Не удалось сохранить данные", Toast.LENGTH_LONG).show();
+        }
     }
 }
