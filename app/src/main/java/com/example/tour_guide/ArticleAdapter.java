2e09e5d3b3fc99ef7d14a6c65278f6b4e1c6c7c3
Spiritfaer <30905488+Spiritfaer@users.noreply.github.com>
2020-12-02 20:13:28 +0200

    Minor update

--- app/src/main/java/com/example/tour_guide/ArticleAdapter.java
+++ app/src/main/java/com/example/tour_guide/ArticleAdapter.java
@@ -1,7 +1,6 @@
 package com.example.tour_guide;
 
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,7 +10,6 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.recyclerview.widget.RecyclerView.ViewHolder;
 
 import java.util.ArrayList;
 
